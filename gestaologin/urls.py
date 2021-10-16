83b4649ba1134324cae8c84ba8984dd645fcc6ce
Roberta Hauser <roberta.hauser@gmail.com>
2021-10-16 14:08:07 -0300

    gestaologin/urls/desativado path PaginaInicial

--- gestaologin/urls.py
+++ gestaologin/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import PaginaInicial
 
 urlpatterns = [
-    path('', PaginaInicial.as_view(),name='index'),
+    #path('', PaginaInicial.as_view(),name='index'),
     path('', auth_views.LoginView.as_view(template_name='index.html'), name='index'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
