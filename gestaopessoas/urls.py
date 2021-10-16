ea4150757c7f30ff487e9ad7e3705c84657f441a
Roberta Hauser <roberta.hauser@gmail.com>
2021-10-16 10:52:30 -0300

    create for-reh-001

--- gestaopessoas/urls.py
+++ gestaopessoas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import for_reh_001r
+from .views import for_reh_001c, for_reh_001r
 
 urlpatterns = [
+    path('for_reh_001c/', for_reh_001c.as_view(), name='for_reh_001c'),
     path('for_reh_001r/', for_reh_001r.as_view(), name='for_reh_001r'),
 ]
--- gestaopessoas/views.py
+++ gestaopessoas/views.py
@@ -1,7 +1,17 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import for_reh_001
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class for_reh_001c(CreateView):
+    #group_required = u"rh1"#somente usuarios do grupo, podem executar
+    model = for_reh_001
+    fields = "__all__"
+    template_name = 'form.html'
+    success_url = reverse_lazy('for_reh_001r')
+
 class for_reh_001r(ListView):
     model = for_reh_001
     template_name = 'for_reh_001r.html'
