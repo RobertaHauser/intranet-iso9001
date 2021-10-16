4edfbf3382d0884425d0ca72cde27f1d5d273446
Roberta Hauser <roberta.hauser@gmail.com>
2021-10-16 13:28:47 -0300

    login index

--- gestaologin/urls.py
+++ gestaologin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import PaginaInicial
 
 urlpatterns = [
     path('', PaginaInicial.as_view(),name='index'),
+    path('', auth_views.LoginView.as_view(template_name='index.html'), name='index'),
 ]
