b8c93834ed165e50189e17f8a658d4523393992f
Roberta Hauser <roberta.hauser@gmail.com>
2021-10-17 09:03:24 -0300

    pip uninstall django-braces

--- gestaopessoas/views.py
+++ gestaopessoas/views.py
@@ -3,11 +3,11 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import for_reh_001
 from django.urls import reverse_lazy
-from braces.views import GroupRequiredMixin
+#from braces.views import GroupRequiredMixin
 
 
 # Create your views here.
-class for_reh_001c(GroupRequiredMixin, CreateView):
+class for_reh_001c(CreateView):
     group_required = u"rh1"#somente usuarios do grupo, podem executar [u"rh1",u"sgq1",...]
     model = for_reh_001
     fields = "__all__"
@@ -19,7 +19,7 @@ class for_reh_001r(ListView):
     template_name = 'for_reh_001r.html'
 
 
-class for_reh_001u(GroupRequiredMixin,UpdateView):
+class for_reh_001u(UpdateView):
     group_required = u"rh1"
     model = for_reh_001
     fields = "__all__"
